2e2d30b53f6df3876eab93140adb707a93f16be7
Gabi Darza <gabidarza@Gabis-MacBook-Pro-2.local>
2024-12-05 15:35:07 -0800

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    
+    import "../app.css"
+    const {children} = $props()
+</script>
+
+<nav>
+    <a href="/">Gabi Darza</a>
+    <div>
+        <a href="/photos">Photos</a>
+        <a href="/info">Info</a>
+    </div>
+</nav>
+
+<main id="main-render-children-inside">
+    {@render children()}
+</main>
+
+<style>
+    nav {
+        color: black;
+        display: flex;
+        justify-content: space-between;
+
+        > div {
+            background-color: lightblue;
+
+            > a {
+                color: black;
+                text-decoration: none;
+
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+    }
+
+</style>
---
+++ src/routes/photos/+page.svelte
@@ -0,0 +1,10 @@
+<img src="/images/kite.jpg" alt="">
+<img src="/images/kite.jpg" alt="">
+<img src="/images/kite.jpg" alt="">
+
+<style>
+    img {
+        max-width: 50vw;
+        height: auto;
+    }
+</style>
