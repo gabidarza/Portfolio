9a1506f11baa59e27f008481eb8226a147744dac
Gabi Darza <gabidarza@gmail.com>
2024-12-05 17:04:56 -0800

    edit to the nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     
+    import "../reset.css"
     import "../app.css"
+
     const {children} = $props()
 </script>
 
@@ -18,21 +20,28 @@
 
 <style>
     nav {
+        background-color: rgba(255,255,255,.75);
+        backdrop-filter: blur(16px);
         color: black;
         display: flex;
         justify-content: space-between;
+        padding: 16px;
 
-        > div {
+        position: sticky;
+        top: 0;
+        left: 0;
+        right: 0;
 
-            > a {
-                color: black;
-                text-decoration: none;
+         a {
+            color: black;
+            text-decoration: none;
 
-                &:hover {
-                    text-decoration: underline;
-                }
+            &:hover {
+                text-decoration: underline;
             }
         }
+
+        
     }
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <h1>Portfolio</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    
+</style>
 
 
